30c667818f50a4a4ccf5e14be8059cbfb28edb76
NISHA NAJIHAH <nishanajihah88@gmail.com>
2025-09-06 22:46:25 +0800

    feat: Update project identifiers and names to reflect new branding for AssetCraft AI

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.assetcraft_ai_v2"
+    namespace = "com.nishanajihah.assetcraftai"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.assetcraft_ai_v2"
+        applicationId = "com.nishanajihah.assetcraftai"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/com/nishanajihah/assetcraftai/MainActivity.kt
+++ android/app/src/main/kotlin/com/nishanajihah/assetcraftai/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.assetcraft_ai_v2
+package com.nishanajihah.assetcraftai
 
 import io.flutter.embedding.android.FlutterActivity
 
